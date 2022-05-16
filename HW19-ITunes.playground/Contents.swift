c75322b2b70d45562457031719fd665ef2bbc66b
Shmorhun Rostyslav <shmorgun4@gmail.com>
2022-05-16 13:52:17 +0300

    1. Bug fixes 2. Created project part 3

--- HW19-ITunes.playground/Contents.swift
+++ HW19-ITunes.playground/Contents.swift
@@ -1,25 +1,6 @@
 import UIKit
 import Foundation
 
-// MARK: - Part 1
-
-var urlComponents = URLComponents(string: "https://itunes.apple.com/search")!
-urlComponents.queryItems = [
-    "term" : "Apple" ,
-    "media" : "software"
-].map { URLQueryItem(name: $0.key, value: $0.value)}
-
-Task {
-    let (data, response) = try await URLSession.shared.data(from: urlComponents.url!)
-    if let httpResponse = response as? HTTPURLResponse,
-       httpResponse.statusCode == 200,
-       let string = String(data: data, encoding: .utf8) {
-//        print(string)
-    }
-}
-
-// MARK: - Part 2
-
 extension Data {
     func prettyPrintedJSONString() {
         guard
@@ -79,8 +60,8 @@ struct StoreItem: Codable{
 
 struct SearchResponse: Codable {
     let results: [StoreItem]
-    
 }
+
 enum StoreItemError: Error, LocalizedError{
     case itemsNotFound
 }
@@ -124,8 +105,3 @@ Task {
         print(error)
     }
 }
-
-
-
-
-
---
+++ iTunes Search (Part 3)/iTunes Search (Part 3)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iTunes Search (Part 3)
+//
+//  Created by Rostyslav Shmorhun on 16.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
